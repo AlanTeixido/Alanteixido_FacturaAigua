f2cd0df7b20b9039c54450df76261787169535e0
alanteixido <alan.teixido.7e7@itb.cat>
2024-01-12 09:37:41 +0100

    Traduit tot al catala, nomes catala.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import org.example.*
 
 /**
- * Este método se encarga de calcular el coste de la factura de l'aigua en función de los litros consumidos y otros elementos.
+ * Aquesta funció es encarrega de calcular el cost de la factura de l'aigua en funció dels litres consumits i altres elements.
  * @author AlanTeixido
  */
 fun main() {
@@ -14,7 +14,7 @@ fun main() {
 
         val esFamiliaNombrosa = readBoolean("$BLUE És família nombrosa? (true/false): $RESET", "Format incorrecte.")
 
-        // Pregunta adicional antes de preguntar por el número de miembros de la familia monomarental
+        // Pregunta addicional abans de preguntar pel nombre de membres de la família monomarental
         val esFamiliaMono = if (esFamiliaNombrosa) false else {
             readBoolean("$BLUE És família monomarental? (true/false): $RESET", "Format incorrecte.")
         }
@@ -22,29 +22,29 @@ fun main() {
         val membresFamilia = if (esFamiliaMono) {
             readInt("$BLUE Introdueix el nombre de membres de la família monomarental: $RESET", "Introdueix un número!", "Valor fora del rang esperat!", 1, 10)
         } else {
-            // Si no es familia monomarental, el número de miembros es irrelevante
+            // Si no és família monomarental, el nombre de membres és irrelevant
             0
         }
 
         val teBoSocial = readBoolean("$BLUE Tens bo social? (true/false): $RESET", "Format incorrecte.")
 
-        // Almacena el resultado en una variable
+        // Emmagatzema el resultat en una variable
         val coste = calcularCostAigua(litresConsumits, esFamiliaNombrosa, esFamiliaMono, teBoSocial, preuXLitre, membresFamilia)
 
-        // Imprime el resultado
+        // Imprimeix el resultat
         println("${WHITE_BACKGROUND_BRIGHT}${BLACK_BOLD}Import Total: ${RESET}$coste €")
     }
 }
 
 /**
- * Calcula el coste de la factura de l'aigua según los parámetros dados.
- * @param litresConsumits Litros de agua consumidos.
- * @param esFamiliaNombrosa Indica si es familia numerosa.
- * @param esFamiliaMono Indica si es familia monomarental.
- * @param teBoSocial Indica si tiene bo social.
- * @param preuXLitre Precio por litro.
- * @param membresFamilia Número de miembros en la familia monomarental.
- * @return Coste total de la factura.
+ * Calcula el cost de la factura de l'aigua segons els paràmetres donats.
+ * @param litresConsumits Litres d'aigua consumits.
+ * @param esFamiliaNombrosa Indica si és família nombrosa.
+ * @param esFamiliaMono Indica si és família monomarental.
+ * @param teBoSocial Indica si té bo social.
+ * @param preuXLitre Preu per litre.
+ * @param membresFamilia Nombre de membres en la família monomarental.
+ * @return Cost total de la factura.
  */
 fun calcularCostAigua(litresConsumits: Int, esFamiliaNombrosa: Boolean, esFamiliaMono: Boolean, teBoSocial: Boolean, preuXLitre: Double, membresFamilia: Int): Double {
     var quotaFixa = 6.0
@@ -56,41 +56,40 @@ fun calcularCostAigua(litresConsumits: Int, esFamiliaNombrosa: Boolean, esFamili
     }
 
     if (esFamiliaNombrosa) {
-        // Descuento del 50% para familia numerosa
+        // Descompte del 50% per a família nombrosa
         quotaVariable *= 0.5
     } else if (esFamiliaMono && membresFamilia > 0) {
-        // Descuento 10x por cada miembro en familia monomarental
+        // Descompte 10x per cada membre en família monomarental
         val descompteMonomarental = 10 * membresFamilia * 0.5 * preuXLitre
         quotaVariable -= descompteMonomarental
     }
 
     if (teBoSocial) {
         quotaFixa = 3.0
-        quotaVariable *= 0.2 // 80% de descuento
+        quotaVariable *= 0.2 // 80% de descompte
     }
 
     val costTotal = quotaFixa + quotaVariable
     mostrarDesglossament(costTotal, quotaFixa, litresConsumits, quotaVariable)
 
-    // Devuelve el coste total
+    // Retorna el cost total
     return costTotal
 }
 
 /**
- * Pide la introducción de los litros consumidos por el cliente.
- * @return Litros consumidos como número entero.
+ * Demana la introducció dels litres consumits pel client.
+ * @return Litres consumits com a número enter.
  */
 fun introduirLitres(): Int {
     return readInt("$BLUE Introdueix els litres gastats: $RESET", "Introdueix un número!", "Valor fora del rang esperat!", 1, 1000)
 }
 
-
 /**
- * Muestra el desglose de la factura con colores.
- * @param costTotal Importe total de la factura de agua.
- * @param quotaFixa Cuota fija mensual.
- * @param litresConsumits Cantidad de litros de agua consumidos.
- * @param quotaVariable Cuota variable en función del consumo.
+ * Mostra el desglossament de la factura amb colors.
+ * @param costTotal Import total de la factura d'aigua.
+ * @param quotaFixa Quota fixa mensual.
+ * @param litresConsumits Quantitat de litres d'aigua consumits.
+ * @param quotaVariable Quota variable en funció del consum.
  */
 fun mostrarDesglossament(costTotal: Double, quotaFixa: Double, litresConsumits: Int, quotaVariable: Double) {
     println("${WHITE_BACKGROUND_BRIGHT}${BLACK_BOLD}Desglossament de la factura d'aigua:${RESET}")
