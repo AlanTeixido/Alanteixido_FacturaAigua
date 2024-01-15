ccea96bfba10cd0280294eaf6ec93d2d6026090d
alanteixido <alan.teixido.7e7@itb.cat>
2024-01-15 12:09:14 +0100

    Commit amb millora del Main.kt i calcularTarifa.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,40 @@
 import org.example.*
 
 /**
- * Aquesta funció es encarrega de calcular el cost de la factura de l'aigua en funció dels litres consumits i altres elements.
+ * Aquesta funció principal es encarrega de calcular el cost de la factura de l'aigua en funció dels litres consumits i altres elements. El màxim de càlculs del cost és de 3.
  * @author AlanTeixido
  */
 fun main() {
-    while (true) {
+    for (i in 1..3) {
         println("\n$WHITE_BACKGROUND_BRIGHT$BLACK_BOLD FACTURA DE L'AIGUA ITB $RESET")
 
-        val preus: DoubleArray = doubleArrayOf(0.15, 0.30)
         val litresConsumits = introduirLitres()
-        val preuXLitre: Double = if (litresConsumits > 200) preus[1] else preus[0]
+        val preuXLitre = calcularTarifa(litresConsumits)
 
         val esFamiliaNombrosa = readBoolean("$BLUE És família nombrosa? (true/false): $RESET", "Format incorrecte.")
-
-        // Pregunta addicional abans de preguntar pel nombre de membres de la família monomarental
-        val esFamiliaMono = if (esFamiliaNombrosa) false else {
-            readBoolean("$BLUE És família monomarental? (true/false): $RESET", "Format incorrecte.")
-        }
+        val esFamiliaMono = if (esFamiliaNombrosa) false else readBoolean("$BLUE És família monomarental? (true/false): $RESET", "Format incorrecte.")
 
         val membresFamilia = if (esFamiliaMono) {
             readInt("$BLUE Introdueix el nombre de membres de la família monomarental: $RESET", "Introdueix un número!", "Valor fora del rang esperat!", 1, 10)
         } else {
-            // Si no és família monomarental, el nombre de membres és irrelevant
             0
         }
 
         val teBoSocial = readBoolean("$BLUE Tens bo social? (true/false): $RESET", "Format incorrecte.")
-
-        // Emmagatzema el resultat en una variable
         val coste = calcularCostAigua(litresConsumits, esFamiliaNombrosa, esFamiliaMono, teBoSocial, preuXLitre, membresFamilia)
+    }
+}
 
+/**
+ * Calcula el cost de la tarifa de l'aigua segons els litres consumits.
+ * @param litresConsumits Litres d'aigua consumits.
+ * @return Cost de la tarifa.
+ */
+fun calcularTarifa(litresConsumits: Int): Double {
+    return when {
+        litresConsumits > 200 -> 0.30
+        litresConsumits in 50..200 -> 0.15
+        else -> 0.0
     }
 }
 
@@ -41,36 +45,28 @@ fun main() {
  * @param esFamiliaMono Indica si és família monomarental.
  * @param teBoSocial Indica si té bo social.
  * @param preuXLitre Preu per litre.
- * @param membresFamilia Nombre de membres en la família monomarental.
+ * @param membresFamilia Nombre de membres a la família monomarental.
  * @return Cost total de la factura.
  */
 fun calcularCostAigua(litresConsumits: Int, esFamiliaNombrosa: Boolean, esFamiliaMono: Boolean, teBoSocial: Boolean, preuXLitre: Double, membresFamilia: Int): Double {
     var quotaFixa = 6.0
-    var quotaVariable: Double = when {
-        litresConsumits < 50 -> 0.0
-        litresConsumits in 50..200 -> litresConsumits * preuXLitre
-        litresConsumits > 200 -> litresConsumits * preuXLitre
-        else -> 0.0
-    }
+    var quotaVariable = litresConsumits * preuXLitre
 
     if (esFamiliaNombrosa) {
-        // Descompte del 50% per a família nombrosa
         quotaVariable *= 0.5
     } else if (esFamiliaMono && membresFamilia > 0) {
-        // Descompte 10x per cada membre en família monomarental
         val descompteMonomarental = 10 * membresFamilia * 0.5 * preuXLitre
         quotaVariable -= descompteMonomarental
     }
 
     if (teBoSocial) {
         quotaFixa = 3.0
-        quotaVariable *= 0.2 // 80% de descompte
+        quotaVariable *= 0.2
     }
 
     val costTotal = quotaFixa + quotaVariable
     mostrarDesglossament(costTotal, quotaFixa, litresConsumits, quotaVariable)
 
-    // Retorna el cost total
     return costTotal
 }
 
